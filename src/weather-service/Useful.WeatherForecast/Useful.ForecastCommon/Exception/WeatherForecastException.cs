ebd1c7fd0271445d84900fff601e4193faabefe5
Ledrunning <ledrunner@hotmail.com>
2024-04-26 22:30:17 +0200

    Added common library

---
+++ src/weather-service/Useful.WeatherForecast/Useful.ForecastCommon/Exception/WeatherForecastException.cs
@@ -0,0 +1,21 @@
+﻿namespace Useful.ForecastCommon.Exception;
+
+public class WeatherForecastException : System.Exception
+{
+    private readonly string? _errorCode;
+    private readonly string? _errorMessage;
+
+    public WeatherForecastException(string message) : base(message)
+    {
+    }
+
+    public WeatherForecastException(string? message, System.Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    public WeatherForecastException(string message, string errorCode, string errorMessage) : base(message)
+    {
+        _errorCode = errorCode;
+        _errorMessage = errorMessage;
+    }
+}

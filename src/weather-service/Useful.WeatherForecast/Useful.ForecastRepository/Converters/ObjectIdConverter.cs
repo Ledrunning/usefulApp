9566a67f9a8f1d1c408fa8752e5e77163c9a09ce
Ledrunning <ledrunner@hotmail.com>
2024-04-27 23:31:56 +0200

    Added ObjectIdConverter

---
+++ src/weather-service/Useful.WeatherForecast/Useful.ForecastRepository/Converters/ObjectIdConverter.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Useful.ForecastRepository.Converters
+{
+    public class ObjectIdConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            if (value.GetType().IsArray)
+            {
+                writer.WriteStartArray();
+                foreach (var item in (Array) value) serializer.Serialize(writer, item);
+                writer.WriteEndArray();
+            }
+            else
+            {
+                serializer.Serialize(writer, value.ToString());
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object? existingValue,
+            JsonSerializer serializer)
+        {
+            var token = JToken.Load(reader);
+            var objectIds = new List<ObjectId>();
+
+            if (token.Type == JTokenType.Array)
+            {
+                objectIds.AddRange(token.ToObject<string[]>().Select(item => new ObjectId(item)));
+                return objectIds.ToArray();
+            }
+
+            if (token.ToObject<string>().Equals("MongoDB.Bson.ObjectId[]"))
+            {
+                return objectIds.ToArray();
+            }
+
+            return new ObjectId(token.ToObject<string>());
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(ObjectId);
+        }
+    }
+}

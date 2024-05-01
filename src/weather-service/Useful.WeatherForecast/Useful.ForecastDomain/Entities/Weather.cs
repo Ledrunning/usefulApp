b68c39010201e4f777d402b23559c9a4b1caa1ee
Ledrunning <ledrunner@hotmail.com>
2024-05-01 21:56:24 +0200

    Added Domain DTO and data base and Weather API configuration

---
+++ src/weather-service/Useful.WeatherForecast/Useful.ForecastDomain/Entities/Weather.cs
@@ -0,0 +1,9 @@
+﻿namespace Useful.ForecastDomain.Entities
+{
+    internal record Weather
+    {
+        public float Temperature { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+    }
+}

2100d8ce4ba858f78f86c02435a427009a1154dd
Ledrunning <ledrunner@hotmail.com>
2024-05-04 23:02:08 +0200

    Added controllers.

--- src/weather-service/Useful.WeatherForecast/Useful.ForecastService/Service/OpenWeatherRestService.cs
+++ src/weather-service/Useful.WeatherForecast/Useful.ForecastService/Service/OpenWeatherRestService.cs
@@ -1,4 +1,5 @@
-﻿using RestSharp;
+﻿using Microsoft.Extensions.Logging;
+using RestSharp;
 using Useful.ForecastService.Contracts;
 using Useful.ForecastService.Models;
 
@@ -8,25 +9,37 @@ public class OpenWeatherRestService : BaseService, IOpenWeatherRestService
 {
     private readonly string _apiKey;
     private readonly IOpenWeatherGeoRestService _geoRestService;
+    private readonly ILogger<OpenWeatherRestService> _logger;
 
-    public OpenWeatherRestService(IOpenWeatherGeoRestService geoRestService, string apiKey, string baseUrl, int timeout)
+    //TODO: Probably I could put here config abstraction
+    public OpenWeatherRestService(ILogger<OpenWeatherRestService> logger, IOpenWeatherGeoRestService geoRestService,
+        string apiKey, string baseUrl, int timeout)
         : base(baseUrl, timeout)
     {
         _geoRestService = geoRestService;
         _apiKey = apiKey;
+        _logger = logger;
     }
 
     //URL - https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
     public async Task<MainWeather> GetWeatherFromOpenWeatherApi(string city, CancellationToken token)
     {
-        var cityInfoList = await _geoRestService.GetCityCoordinates(city, token);
-        var cityInfo = cityInfoList.Select(data => data).FirstOrDefault();
+        try
+        {
+            var cityInfoList = await _geoRestService.GetCityCoordinates(city, token);
+            var cityInfo = cityInfoList.Select(data => data).FirstOrDefault();
 
-        var url = new Uri($"{BaseUrl}lat={cityInfo?.Lat}&lon={cityInfo?.Lon}&appid={_apiKey}");
-        var client = new RestClient(SetOptions(url));
-        var request = new RestRequest();
-        var response = await client.ExecuteAsync(request, token);
+            var url = new Uri($"{BaseUrl}lat={cityInfo?.Lat}&lon={cityInfo?.Lon}&appid={_apiKey}");
+            var client = new RestClient(SetOptions(url));
+            var request = new RestRequest();
+            var response = await client.ExecuteAsync(request, token);
 
-        return GetContent<MainWeather>(response, url.AbsoluteUri);
+            return GetContent<MainWeather>(response, url.AbsoluteUri);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("Error retrieve data from OpenWeatherApi {e}:", e);
+            throw;
+        }
     }
 }

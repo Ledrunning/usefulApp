b3463b48ee8984945616a07a578acb5bb387d3ec
Ledrunning <ledrunner@hotmail.com>
2024-05-13 21:36:07 +0200

    Added Task classes

---
+++ src/weather-service/Useful.WeatherForecast/Useful.ForecastTaskScheduler/ForecastScheduler.cs
@@ -0,0 +1,14 @@
+﻿using Hangfire;
+
+namespace Useful.ForecastTaskScheduler
+{
+    public class ForecastScheduler
+    {
+        //"0 */2 * * * *" - 2 min. cron expression for debugging 
+        public void ScheduleForecastDataUpdate(CancellationToken token)
+        {
+            // Update every 12 hours
+            //RecurringJob.AddOrUpdate<ForecastDataUploader>(nameof(ForecastDataUploader), updater => updater.Update(token), Cron.Daily);
+        }
+    }
+}

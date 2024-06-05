89b1f31a1a9bee35c8c3d115bee00c035a3630be
Ledrunning <ledrunner@hotmail.com>
2024-06-05 22:24:54 +0200

    Added common project and created service for retrieving data from coincap API

---
+++ src/cryptocurrency-service/Useful.CryptoCurrency/Useful.CryptoCurrencyService/Contracts/ICryptocurrencyRatesService.cs
@@ -0,0 +1,13 @@
+﻿using Useful.CryptoCurrencyService.Dto;
+
+namespace Useful.CryptoCurrencyService.Contracts;
+
+public interface ICryptoCurrencyRatesService
+{
+    /// <summary>
+    ///     Get all crypto rates from coincap
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns>list of RateMode?l</returns>
+    Task<IList<Data>?> GetAllCryptoCurrencyRatesAsync(CancellationToken token);
+}

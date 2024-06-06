974c3cad95043d1d4029d0293b10088f0cecb94e
Ledrunning <ledrunner@hotmail.com>
2024-06-06 21:22:17 +0200

    Added configuration for concap

---
+++ src/cryptocurrency-service/Useful.CryptoCurrency/Useful.CryptoCurrencyCommon/Contracts/ICryptoCurrencyGatewayConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Useful.CryptoCurrencyCommon.Contracts;
+
+public interface ICryptoCurrencyGatewayConfiguration
+{
+    public string? BaseUrl { get; set; }
+    public int? Timeout { get; set; }
+}
---
+++ src/cryptocurrency-service/Useful.CryptoCurrency/Useful.CryptoCurrencyGateway/Controllers/CryptoCurrencyController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Useful.CryptoCurrencyGateway.Controllers
+{
+    public class CryptoCurrencyController : Controller
+    {
+        // GET: CryptoCurrencyController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CryptoCurrencyController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CryptoCurrencyController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CryptoCurrencyController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CryptoCurrencyController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CryptoCurrencyController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CryptoCurrencyController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CryptoCurrencyController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
